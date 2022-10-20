6f76c20c2b2202e1f24084ecf789b7d23c433049
Alexander Dietrich <alexander@dietrich.cx>
2022-10-20 23:24:19 +0200

    Add "show" and "update" commands

--- surgeprotector.py
+++ surgeprotector.py
@@ -9,25 +9,37 @@ import click
 import psutil
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option('--number', '-n',
+              help='Number of IP addresses to show.',
+              default=10,
+              show_default=True)
+def show(number):
+    """Show IP addresses with the most TCP connections."""
+    connections = get_connections()
+    connections = [(v, k) for k, v in connections.items()]
+    connections.sort(key=lambda conn: conn[0])
+    for conn in connections[-number:]:
+        print(f'{conn[0]:6} {conn[1]}')
+
+
+@main.command()
+@click.argument('output', type=click.Path(dir_okay=False, allow_dash=True))
 @click.argument('limit', type=int)
-@click.option('--output', '-o',
-              help='Write "ExitPolicy reject" lines to this file.',
-              type=click.Path(dir_okay=False, allow_dash=True),
-              default='-')
 @click.option('--command', '-c',
               help='Execute this command to reload Tor configuration.')
-def main(limit, output, command):
-    """Generate "ExitPolicy reject" lines for flooded remote IP addresses.
+def update(limit, output, command):
+    """Update "ExitPolicy" lines for flooded IP addresses in OUTPUT.
 
-    An address is considered flooded over LIMIT TCP connections.
+    An IP address is considered flooded over LIMIT TCP connections.
     """
     timestamp = int(time.time())
-    connections = defaultdict(int)
-    for conn in psutil.net_connections(kind='tcp'):
-        if not conn.raddr:
-            continue
-        connections[conn.raddr.ip] += 1
+    connections = get_connections()
     connections = {k: v for k, v in connections.items() if v > limit}
     if not connections:
         return
@@ -40,6 +52,16 @@ def main(limit, output, command):
             f.write(line)
     if command:
         os.system(command)
+
+
+def get_connections():
+    """Return a dict of IP address to TCP connection count."""
+    connections = defaultdict(int)
+    for conn in psutil.net_connections(kind='tcp'):
+        if not conn.raddr:
+            continue
+        connections[conn.raddr.ip] += 1
+    return connections
 
 
 if __name__ == '__main__':
